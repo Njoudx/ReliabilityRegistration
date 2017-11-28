e2636ef6e5de38f89edf95f05d8a0ea34751cb07
Njoud <njoudiix@gmail.com>
2017-11-28 10:38:56 +0300

    00

---
+++ ReliabilityRegistration/ReliabilityRegistration/Models/RegistrationDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ReliabilityRegistration.Models
+{
+    
+        public class HelpdeskDbContext : DbContext
+        {
+            public DbSet<Registeration> Users { get; set; }
+            public HelpdeskDbContext() : base("name=DefaultConnection")
+            {
+
+            }
+
+            public static HelpdeskDbContext Create()
+            {
+                return new HelpdeskDbContext();
+            }
+        }
+}
